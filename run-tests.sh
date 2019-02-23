f618c31ea35e5308f32dfab3fcf92c599e1742c5
Mirek Simek <miroslav.simek@vscht.cz>
2019-02-23 15:57:43 +0100

    name refactor

--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,7 @@
 # OArepo Files REST is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-pydocstyle oarepo_files_rest tests docs && \
+pydocstyle invenio_oarepo_files_rest tests docs && \
 isort -rc -c -df && \
 check-manifest --ignore ".travis-*,docs/_build*" && \
 sphinx-build -qnNW docs docs/_build/html && \
